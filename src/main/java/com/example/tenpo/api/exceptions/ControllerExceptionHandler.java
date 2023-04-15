350ef4a120b78d732c0574dfce67fe9f473125f4
Gonzalo Lopez <gonzolopez@outlook.com.ar>
2023-04-15 19:23:04 -0300

    Agrego handler

--- src/main/java/com/example/tenpo/api/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/example/tenpo/api/exceptions/ControllerExceptionHandler.java
@@ -12,12 +12,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ControllerExceptionHandler{
 
-    @ExceptionHandler(NoHandlerFoundException.class)
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ApiError handleException(HttpServletRequest request, Exception ex) {
+        return new ApiError("internal_server_error", "An error occurred while processing your request.", HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+   /* @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
     public ApiError noHandlerFoundException(HttpServletRequest request, NoHandlerFoundException ex) {
         return new ApiError("route_not_found", String.format("Route %s not found.", request.getRequestURI()),
                 HttpStatus.NOT_FOUND.value());
-    }
+    }*/
 
 }
